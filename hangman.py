b57e6aab6b2a1cdc9acfcdbe9d2c87bbc1dc4b52
Teslaboi3000 <matteo.caputo2014@gmail.com>
2023-09-23 10:52:42 +0200

    Added feature that shows the word in hangman

--- hangman.py
+++ hangman.py
@@ -1,4 +1,5 @@
 import random
+
 words = [
     "cat",
     "dog",
@@ -49,18 +50,30 @@ words = [
     "ice cream",
     "chocolate",
 ]
+
 word_to_guess = random.choice(words)
-print("Welcome to my hangman!\nLet's play!\n")
+revealed_word = [word_to_guess[0]] + ["_"] * (len(word_to_guess) - 2) + [word_to_guess[-1]]
+lives = 5
+
+print("Welcome to my Hangman game!\nLet's play!\n")
+
 while True: 
-    lives = 5
-    first_letter = word_to_guess[0]
-    last_letter = word_to_guess[-1]
     print(f'{len(word_to_guess)} letters')
-    print(f"The first letter of the word is {first_letter} and the last letter is {last_letter}\n")
+    print(f"The word so far is {''.join(revealed_word)}\n")
     guess = input('> ')
     if guess == word_to_guess:
         print("You got it! GG, let's change word!\n")
         word_to_guess = random.choice(words)
+        revealed_word = [word_to_guess[0]] + ["_"] * (len(word_to_guess) - 2) + [word_to_guess[-1]]
+        lives = 5
     else:
         lives -= 1
-        print(f"Sorry to say but Wrong! You still have {lives} lives\n")
+        if lives > 0:
+            for i in range(1, len(word_to_guess) - 1):
+                if revealed_word[i] == "_":
+                    revealed_word[i] = word_to_guess[i]
+                    break
+            print(f"Sorry to say but Wrong! You still have {lives} lives\n")
+        else:
+            print("Game over! You've run out of lives.\n")
+            break
