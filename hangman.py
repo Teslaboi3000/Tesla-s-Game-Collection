94492abe1062c78511a91de70e91a68cada16ffe
Teslaboi3000 <matteo.caputo2014@gmail.com>
2023-09-23 10:45:30 +0200

    Small changes to Hangman

--- hangman.py
+++ hangman.py
@@ -53,10 +53,14 @@ word_to_guess = random.choice(words)
 print("Welcome to my hangman!\nLet's play!\n")
 while True: 
     lives = 5
+    first_letter = word_to_guess[0]
+    last_letter = word_to_guess[-1]
     print(f'{len(word_to_guess)} letters')
+    print(f"The first letter of the word is {first_letter} and the last letter is {last_letter}\n")
     guess = input('> ')
     if guess == word_to_guess:
         print("You got it! GG, let's change word!\n")
+        word_to_guess = random.choice(words)
     else:
         lives -= 1
         print(f"Sorry to say but Wrong! You still have {lives} lives\n")
