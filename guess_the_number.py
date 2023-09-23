29d3c7f6338d8e31a2c8fb695e04df0549a4af18
Teslaboi3000 <matteo.caputo2014@gmail.com>
2023-09-23 10:20:30 +0200

    Added point system to Guess The Number

--- guess_the_number.py
+++ guess_the_number.py
@@ -2,7 +2,8 @@ import random
 
 number = random.randint(0, 100)
 
-print('Welcome to my number guessing game! You can start!\n')
+print('Welcome to my number guessing game! You win when you get to 50 points You can start!\n')
+points = 0
 
 while True:
     try:  
@@ -14,5 +15,13 @@ while True:
         else:
             print("You guessed! Let's change number!\n")
             number = random.randint(0, 100)
+            points += 1 
+            if points == 1:
+                print(f"You get a point! You now are at {points} point!")
+            else:
+                print(f"You get a point! You now are at {points} points!")
     except ValueError:
         print("I'm pretty sure that isn't the number dude")
+
+    if points == 50:
+        print("GG, You win! See you next time!")
