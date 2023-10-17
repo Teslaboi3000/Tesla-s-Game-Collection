b591cacbccc46327592252a36ef3af42d0cc2475
Teslaboi <teslaboi@DESKTOP-TUACTLB.Home>
2023-10-17 21:37:31 +0200

    Small fix to the Two Truths One Lie game

--- 2truths1lie.py
+++ 2truths1lie.py
@@ -12,7 +12,7 @@ while True:
         win_messages = ["Nice I got it!\n", "GG I guessed it!\n", "I'm so lucky! I got it!\n", "No way! I still can't believe it! I got it!\n", "If you lied I'm so angry with you! If not I got it nice\n"]
         print(random.choice(win_messages))
     elif is_lie_correct == "no":
-        loose_messages = ["Damn, ill have more luck next time!", "OH I GOT I- oh I din't lol", "Crap I got it wrong, maybe next time"]
+        loose_messages = ["Damn, ill have more luck next time!\n", "OH I GOT I- oh I din't lol\n", "Crap I got it wrong, maybe next time\n"]
         print(random.choice(loose_messages))
     else:
         is_lie_correct = input("I din't understand, did I win? ").lower()
